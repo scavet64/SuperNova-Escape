dec328dcc282732ba5f76652944a19d1ae905a08
scavet64 <scavet64@students.rowan.edu>
2017-02-25 21:42:08 -0500

    Missed a bunch of files

---
+++ Assets/TapController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapController : MonoBehaviour {
+
+    private Animator TapAnimator;
+    private Image image;
+
+    // Use this for initialization
+    void Start () {
+        TapAnimator = this.GetComponent<Animator>();
+        image = GetComponent<Image>();
+        float firstRock = Spawner.currentSpawnedRocks[0].transform.position.x;
+        if(firstRock < 0) {
+            //rock is on left
+            this.transform.position += new Vector3(270, -419);
+        } else {
+            //rock is on right
+            this.transform.position += new Vector3(-270, -419);
+        }
+
+        image.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void fadeAway() {
+        TapAnimator.Play("FadeAway");
+    }
+}
